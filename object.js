95de8b8e83d3f7db63598e793c9d3424dbe748c6
ishrat <siddiqueishrat36@gmail.com>
2025-07-24 18:56:51 +0500

    Add files via upload

---
+++ object.js
@@ -0,0 +1,62 @@
+function Doctor(name, email, age, grade){
+    this.name = name;
+    this.email = email;
+    this.age = age;
+    this.grade = grade;
+
+}
+
+  var drDetails = [
+    new Doctor("DR,ARIF", "arif123@gmail.com", 11, 3),
+    new Doctor("DR,NIHAL", "nihalkhan@gmail.com", 9, 2),
+    new Doctor("DR,Fasiulla Mir", "fasi142@gmail.com", 22, 11),
+    new Doctor("DR,ALI", "ali978@gmail.com", 12, 6),
+    new Doctor("DR,Kosaer", "kosar@gmail.com", 24, 11),
+    new Doctor("DR,Surname", "surname@gmail.com", 23, 12),
+    new Doctor("DR,Jay KUMAR", "kumar266@gmail.com", 24, 11),
+    new Doctor("DR, KAUSaR Tahir", "kausar@gmail.com", 22, 7),  
+     new Doctor("DR,Shahin Zafar", "zafarsha@gmail.com", 25, 15),
+     new Doctor("DR,Mushtaq", "mushtaq@gmail.com", 20, 10),
+    new Doctor("DR,Ehtesham", "ehtesham@gmail.com", 24, 11),
+   new Doctor("DR,Musarrat", "musarrat@gmail.com", 22, 11),
+    new Doctor("DR,Salimullah", "salimillah185@gmail.com", 30, 15),
+   new Doctor("DR,Faizan", "faizan@gmail.com", 20, 9),
+  new Doctor("DR,Farhia", "farhia@gmail.com", 26, 13),
+   new Doctor("DR,Wasifa", "wasifa@gmail.com", 26, 12),
+   new Doctor("DR,Mehwis", "mehwish@gmail.com", 21, 11),
+   new Doctor("DR,Afifa", "afifa@gmail.com", 20, 9),
+    new Doctor("DR,Ibsham", "ibsham184@gmail.com", 25, 14),
+    new Doctor("DR,Uriba", "uriba@gmail.com", 23, 10),
+    new Doctor("DR,Raima", "raima@gmail.com", 25, 13),
+    new Doctor("DR,Rifa", "raif@gmail.com", 10, 5),
+    new Doctor("DR,Amima", "amima@gmail.com", 25, 11),
+    new Doctor("DR,Raif", "raif@gmail.com", 22, 11),
+    new Doctor("DR,Raznal", "raznal@gmail.com", 24, 12),
+    new Doctor("DR,Shazim", "shazim@gmail.com", 20,10),
+    new Doctor("DR,Anas", "anas445@gmail.com", 26, 13),
+    new Doctor("DR,Samina", "samina@gmail.com", 10, 5),
+    new Doctor("DR,Halema", "halema@gmail.com", 12,6),
+    new Doctor("DR,Uzma", "uzma@gmail.com", 22,12),
+    new Doctor("DR,Zehra", "zehra@gmail.com", 17, 7),
+    new Doctor("DR,Rizwan", "rizwan@gmail.com", 22, 12),
+    new Doctor("DR,Raheel", "raheel@gmail.com", 27, 13),
+    new Doctor("DR,Kiran", "kiran@gmail.com", 12, 6),
+    new Doctor("DR,Rimsha", "rimsha@gmail.com", 24, 11),
+    new Doctor("DR,Awaes", "awaes@gmail.com", 14, 9),
+    new Doctor("DR,Aeman", "aeman@gmail.com",11, 5),
+    new Doctor("DR,Rida", "rida@gmail.com", 15, 7),
+     new Doctor("DR,Erum", "erum@gmail.com", 21, 9),
+     new Doctor("DR,Rubab", "rubab234@gmail.com", 25, 12),
+     new Doctor("DR,Kainat", "kainat@gmail.com", 22, 8),
+     new Doctor("DR,Fiza", "fiza266@gmail.com", 21, 9),
+     new Doctor("DR,Hina", "hina321@gmail.com", 27, 7),
+     new Doctor("DR,Noman", "noman421@gmail.com", 16, 6),
+     new Doctor("DR,Rehan", "rehan.2@gmail.com", 26, 14),
+     new Doctor("DR,Abdul Rehman", "rehman098@gmail.com", 26, 11),
+       new Doctor("DR,Abdul Hameed", "abdul786@gmail.com", 25, 13),
+       new Doctor("DR,Adil", "adil66@gmail.com", 32, 11),
+        new Doctor("DR,Aeni", "aeni456@gmail.com", 33,16),
+         new Doctor("DR,AFSHA", "afsha@gmail.com", 24, 12),  
+                                         
+  ]
+  console.log(drDetails);
